ff7202538fcd99875d7fd04982e74adeb1178e8b
TheLastVelo <jarellchinn44@gmail.com>
2023-07-15 09:47:38 -0700

    merge conflicts to get justins code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,18 @@
+// API that displays a random Breaking Bad Quote
+
+function breakingBadQuote() {
+  var requestURL = "https://api.breakingbadquotes.xyz/v1/quotes";
+  fetch(requestURL)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+    });
+}
+
+// breakingBadQuote();
+
 function unsplashAPI() {
   var requestURL =
     "https://api.unsplash.com/photos/random?client_id=Ab4F25pH3_s49oNWOzNXoahqu18przepQm1JgDMKkZA";
@@ -12,6 +27,3 @@ function unsplashAPI() {
 }
 
 unsplashAPI();
-
-var APIkey = "Ab4F25pH3_s49oNWOzNXoahqu18przepQm1JgDMKkZA";
-var secretKey = "syAWQL1pyzZhz7yIStoCCZ0GfXUqPgQnPrN07A69NNw";
