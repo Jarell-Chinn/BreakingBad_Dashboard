0b9341108b2df48d8ad6a07f6ada7eeae008d265
TheLastVelo <jarellchinn44@gmail.com>
2023-07-15 10:55:05 -0700

    added random background image

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,17 +13,46 @@ function breakingBadQuote() {
 
 // breakingBadQuote();
 
-function unsplashAPI() {
-  var requestURL =
-    "https://api.unsplash.com/photos/random?client_id=Ab4F25pH3_s49oNWOzNXoahqu18przepQm1JgDMKkZA";
+// function unsplashAPI() {
+//   var requestURL =
+//     "https://api.unsplash.com/photos/random?client_id=oeF6_nedrYDZgPYD3W22C9NSsJsCa0DeZfWTlHO7u2I";
 
-  fetch(requestURL)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      console.log(data);
-    });
-}
+//   fetch(requestURL)
+//     .then(function (response) {
+//       response.json();
+//     })
+//     .then(function (data) {
+//       console.log(data);
+//     });
+// }
 
 unsplashAPI();
+// https://api.unsplash.com/search/collections?page=1&query=office
+
+var apiKey = "Ab4F25pH3_s49oNWOzNXoahqu18przepQm1JgDMKkZA";
+var apiKey2 = "oeF6_nedrYDZgPYD3W22C9NSsJsCa0DeZfWTlHO7u2I";
+
+// Make a GET request to Unsplash API for a random photo
+fetch(
+  "https://api.unsplash.com/photos/random?query=new-mexico&client_id=" + apiKey
+)
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data);
+
+    var photoUrl = data.urls.regular;
+
+    // Create an <img> element and set its source to the random photo URL
+    var imageElement = document.body.setAttribute(
+      "style",
+      "background-image:url(" + photoUrl + ")"
+    );
+    // imageElement.src = photoUrl;
+
+    // Append the image element to a container in the HTML
+    // var container = document.getElementById("backgroundimg");
+    // container.appendChild(imageElement);
+  })
+  .catch((error) => {
+    console.error("Error:", error);
+  });
